0700a1c69d53ae54dcd1c6cbe501b22db6e29a6d
Olga Pyryeva <vashkevicholga@gmail.com>
2022-03-19 16:36:03 +0300

    add(requestCall.js): add open modal window for request call

--- src/modules/requestСall.js
+++ src/modules/requestСall.js
@@ -1,4 +1,36 @@
+import { animate } from "./helpers";
+
 const requestCall = () => {
-  // console.log("requestCall");
+  const requestBtn = document.querySelector("div.button > a");
+  const requestModal = document.querySelector(".header-modal");
+  const modalFone = document.querySelector(".overlay");
+  const modalCloseBtn = document.querySelector(".header-modal__close");
+
+  const showModal = () => {
+    requestModal.style.display = "block";
+    modalFone.style.display = "block";
+
+    animate({
+      duration: 500,
+      timing(timeFraction) {
+        return timeFraction;
+      },
+      draw(progress) {
+        requestModal.style.opacity = 1 * progress;
+        modalFone.style.opacity = 1 * progress;
+      },
+    });
+  };
+  const hideModal = () => {
+    requestModal.style.display = "";
+    modalFone.style.display = "";
+  };
+
+  requestBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    showModal();
+  });
+
+  modalCloseBtn.addEventListener("click", hideModal);
 };
 export default requestCall;
