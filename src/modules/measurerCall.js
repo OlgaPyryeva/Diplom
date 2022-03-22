33f7e0bf0e4b06feafec97c0fdbcf97b229abf19
Olga Pyryeva <vashkevicholga@gmail.com>
2022-03-22 23:07:16 +0300

    fix(measurerCall.js): fix open modal for call measurer

--- src/modules/measurerCall.js
+++ src/modules/measurerCall.js
@@ -1,24 +1,40 @@
+import { animate } from "./helpers";
+
 const measurerCall = () => {
-  const measurerCallBtn = document.querySelector(".btn-sm");
+  const measurerCallBtn = document.querySelectorAll(".btn-sm");
   const servicesModal = document.querySelector(".services-modal");
   const modalFone = document.querySelector(".overlay");
   const servicesModalCloseBtn = document.querySelector(
     ".services-modal__close"
   );
 
+  measurerCallBtn.forEach((element) => {
+    element.addEventListener("click", (e) => {
+      e.preventDefault();
+      showServicesModal();
+    });
+  });
+
   const showServicesModal = () => {
     servicesModal.style.display = "block";
     modalFone.style.display = "block";
+
+    animate({
+      duration: 500,
+      timing(timeFraction) {
+        return timeFraction;
+      },
+      draw(progress) {
+        servicesModal.style.opacity = 1 * progress;
+        modalFone.style.opacity = 1 * progress;
+      },
+    });
   };
 
   const hideServicesModal = () => {
     servicesModal.style.display = "";
     modalFone.style.display = "";
   };
-  measurerCallBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    showServicesModal();
-  });
 
   servicesModalCloseBtn.addEventListener("click", hideServicesModal);
 };
