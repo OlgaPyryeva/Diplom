f74cecda0bb831707c91ff7d38da62e2665968b4
Olga Pyryeva <vashkevicholga@gmail.com>
2022-03-24 23:14:27 +0300

    add(scrollBtn.js): add scroll

--- src/modules/scrollBtn.js
+++ src/modules/scrollBtn.js
@@ -1,4 +1,27 @@
 const scrollBtn = () => {
-  // console.log("scroll");
+  const scrollImg = document.querySelector("div.smooth-scroll>img");
+  const scrollBlock = document.querySelector("div.smooth-scroll");
+  const beginPage = document.getElementById("header");
+  const nav = document.getElementById("navigation");
+  const firstPart = document.getElementById("offer");
+  let windowScrollTop = 0;
+
+  const heightFirstBlock =
+    beginPage.clientHeight + nav.clientHeight + firstPart.clientHeight;
+
+  window.addEventListener("scroll", (e) => {
+    windowScrollTop = window.scrollY;
+    if (windowScrollTop <= heightFirstBlock || windowScrollTop === 0) {
+      scrollBlock.style.opacity = "0";
+    }
+    if (windowScrollTop > heightFirstBlock) {
+      scrollBlock.style.opacity = "1";
+    }
+  });
+
+  scrollImg.addEventListener("click", (e) => {
+    e.preventDefault();
+    beginPage.scrollIntoView({ alignToTop: true, behavior: "smooth" });
+  });
 };
 export default scrollBtn;
