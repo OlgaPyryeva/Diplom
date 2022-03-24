09cecdc1105e1c2a30f241c940b96de68133b3e9
Olga Pyryeva <vashkevicholga@gmail.com>
2022-03-24 18:58:49 +0300

    add(sertificate.js): add modal for sertificate

--- src/modules/sertificate.js
+++ src/modules/sertificate.js
@@ -1,5 +1,29 @@
 const sertificate = () => {
-  const sertificateDocument = document.querySelector(".sertificate-document");
-  // console.log(sertificateDocument);
+  const sertificateDocuments = document.querySelectorAll(
+    ".sertificate-document"
+  );
+
+  sertificateDocuments.forEach((sertificate) => {
+    const sertificateImg = sertificate.href;
+
+    sertificate.addEventListener("click", (e) => {
+      e.preventDefault();
+      let sertificateModal = document.createElement("div");
+      sertificateModal.className = "sertificate-modal";
+      sertificateModal.style.backgroundImage = `url(${sertificateImg})`;
+      sertificateModal.style.display = "block";
+      sertificateModal.innerHTML = `
+      <span title="Close" class="sertificate-modal__close">x</span></div>`;
+
+      const documentsBlock = document.getElementById("documents");
+
+      documentsBlock.append(sertificateModal);
+
+      const closeBtn = document.querySelector(".sertificate-modal__close");
+      closeBtn.addEventListener("click", () => {
+        sertificateModal.remove();
+      });
+    });
+  });
 };
 export default sertificate;
